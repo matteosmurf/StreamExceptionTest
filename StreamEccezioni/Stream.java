8828e6bbfe57bfe52eb979c304ce3601f1fad400
matteo <matteo.antonelli06@gmail.com>
2024-12-19 09:40:42 +0100

    aggiuntoTestCreaFile

--- StreamEccezioni/Stream.java
+++ StreamEccezioni/Stream.java
@@ -2,10 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.io.File;
 
 public class Stream {
 
-
+    /* 
+     * Test stream Scanner.in
+     */
     public static void wScanner(){
         System.out.println("w Scanner start");
         Scanner in = new Scanner(System.in);
@@ -19,7 +22,9 @@ public class Stream {
         System.out.println("w Scanner end \n \n");
     }
 
-
+    /* 
+     * Test BufferedReader
+     */
     public static void wBufferedReader(){
         System.out.println("w BufferedReader start");
         BufferedReader inR = new BufferedReader(new InputStreamReader(System.in));
@@ -38,4 +43,28 @@ public class Stream {
         }
         System.out.println("w BufferedReader end");
     }
+
+
+    /* 
+     * Test create file
+     */
+    public static void mkFile() {
+        try {
+            File fileTest = new File("fileTest");
+            if (fileTest.createNewFile()){
+                System.out.println("File creato:");
+            } else {
+                System.err.println("Il file già esiste");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /* 
+     * Test Write File
+     */
+    
+
 }
--- StreamEccezioni/Test.java
+++ StreamEccezioni/Test.java
@@ -4,10 +4,10 @@
 
 public class Test {
     public static void main(String[] args){
-        Stream streamBufferedReader = new Stream();
 
-        streamBufferedReader.wScanner();
-        streamBufferedReader.wBufferedReader();
-    }
+        Stream.wScanner();
+        Stream.wBufferedReader();
+        Stream.mkFile();
 
+    }   
 }
